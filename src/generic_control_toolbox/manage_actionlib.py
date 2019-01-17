d9978314f1d1f65d58355bb3219a07a05865af43
Diogo Almeida <diogoa@kth.se>
2019-01-17 11:18:08 +0100

    autopep8

--- src/generic_control_toolbox/manage_actionlib.py
+++ src/generic_control_toolbox/manage_actionlib.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-import rospy
 import sys
-import actionlib
 
+import actionlib
+import rospy
 """
     @package manage_actionlib
     This module provides utility methods for setting up actionlib calls through an overarching actionlib server.
 """
 
-def monitor_action_goal(action_server, action_client, action_goal, action_name = "current action", time_limit = float("inf"), abort_on_fail = False):
+
+def monitor_action_goal(action_server,
+                        action_client,
+                        action_goal,
+                        action_name="current action",
+                        time_limit=float("inf"),
+                        abort_on_fail=False):
     """Send and monitor an action goal to a given action client.
 
        The monitor will return in case of the client reporting success, preemption or
@@ -26,45 +32,48 @@ def monitor_action_goal(action_server, action_client, action_goal, action_name =
     action_client.send_goal(action_goal)
     init_time = rospy.Time.now()
     while action_server.is_active():
-       if (rospy.Time.now() - init_time).to_sec() > time_limit:
-           rospy.logwarn("Timeout of request")
-           action_client.cancel_goal()
-           success = False
-           break
+        if (rospy.Time.now() - init_time).to_sec() > time_limit:
+            rospy.logwarn("Timeout of request")
+            action_client.cancel_goal()
+            success = False
+            break
 
-       if action_server.is_preempt_requested():
-           rospy.logwarn("Preempting " + action_name)
-           action_client.cancel_goal()
-           finished = action_client.wait_for_result(timeout = rospy.Duration(1.0))
+        if action_server.is_preempt_requested():
+            rospy.logwarn("Preempting " + action_name)
+            action_client.cancel_goal()
+            finished = action_client.wait_for_result(
+                timeout=rospy.Duration(1.0))
 
-           if not finished:
-               rospy.logerr(action_name + " failed to preempt! This should never happen")
-               action_server.set_aborted(text = "Aborted due to action " + action_name + " failing to preempt")
-           else:
-               action_server.set_preempted(text = "Preempted while running " + action_name)
-               rospy.loginfo("Preempted while running")
-           break
+            if not finished:
+                rospy.logerr(action_name +
+                             " failed to preempt! This should never happen")
+                action_server.set_aborted(text="Aborted due to action " +
+                                          action_name + " failing to preempt")
+            else:
+                action_server.set_preempted(
+                    text="Preempted while running " + action_name)
+                rospy.loginfo("Preempted while running")
+            break
 
-       if action_client.get_state() == actionlib.GoalStatus.ABORTED:
-        rospy.logerr(action_name + " aborted!")
-        success = False
-        if abort_on_fail:
-            action_server.set_aborted(text = action_name + " aborted")
-        break
+        if action_client.get_state() == actionlib.GoalStatus.ABORTED:
+            rospy.logerr(action_name + " aborted!")
+            success = False
+            if abort_on_fail:
+                action_server.set_aborted(text=action_name + " aborted")
+            break
 
-       if action_client.get_state() == actionlib.GoalStatus.PREEMPTED:
-        rospy.logerr(action_name + " preempted!")
-        success = False
-        if abort_on_fail:
-            action_server.set_aborted(text = action_name + " was preempted")
-        break
+        if action_client.get_state() == actionlib.GoalStatus.PREEMPTED:
+            rospy.logerr(action_name + " preempted!")
+            success = False
+            if abort_on_fail:
+                action_server.set_aborted(text=action_name + " was preempted")
+            break
 
-       if action_client.get_state() == actionlib.GoalStatus.SUCCEEDED:
-        rospy.loginfo(action_name + " succeeded!")
-        success = True
-        break
+        if action_client.get_state() == actionlib.GoalStatus.SUCCEEDED:
+            rospy.loginfo(action_name + " succeeded!")
+            success = True
+            break
 
-
-       rospy.sleep(0.1)
+        rospy.sleep(0.1)
 
     return success
