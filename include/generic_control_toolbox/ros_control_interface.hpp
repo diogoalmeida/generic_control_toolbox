b15ae41c9280af618dfa564c508822e5ffb8d8e6
Diogo Almeida <diogoa@kth.se>
2019-04-16 10:29:43 +0200

    Complete init and starting methods

--- include/generic_control_toolbox/ros_control_interface.hpp
+++ include/generic_control_toolbox/ros_control_interface.hpp
@@ -3,6 +3,7 @@
 
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <urdf/model.h>
 #include <generic_control_toolbox/controller_template.hpp>
 
 namespace generic_control_toolbox
@@ -33,7 +34,13 @@ class RosControlInterface
   ros::NodeHandle& nh_;
   ControllerBase controller_;
   JointType joint_type_;
+  unsigned int n_joints_;
+  std::vector<std::string> joint_names_;
+  std::vector<urdf::JointConstSharedPtr> joint_urdfs_;
+  std::vector<hardware_interface::JointHandle> joints_;
 };
+
+// Implementation in header file due to being a template class.
 
 template <class JointInterface>
 RosControlInterface<JointInterface>::RosControlInterface(
@@ -69,11 +76,63 @@ template <class JointInterface>
 bool RosControlInterface<JointInterface>::init(JointInterface* hw,
                                                ros::NodeHandle& nh)
 {
+  // joint initialization from
+  // https://github.com/ros-controls/ros_controllers/blob/melodic-devel/effort_controllers/src/joint_group_position_controller.cpp
+
+  // List of controlled joints
+  std::string param_name = "joints";
+  if (!nh.getParam(param_name, joint_names_))
+  {
+    ROS_ERROR_STREAM("Failed to getParam '" << param_name << "' (namespace: "
+                                            << nh.getNamespace() << ").");
+    return false;
+  }
+
+  n_joints_ = joint_names_.size();
+
+  if (n_joints_ == 0)
+  {
+    ROS_ERROR_STREAM("List of joint names is empty.");
+    return false;
+  }
+
+  // Get URDF
+  urdf::Model urdf;
+  if (!urdf.initParam("robot_description"))
+  {
+    ROS_ERROR("Failed to parse urdf file");
+    return false;
+  }
+
+  for (unsigned int i = 0; i < n_joints_; i++)
+  {
+    const std::string& name = joint_names_[i];
+
+    try
+    {
+      joints_.push_back(hw->getHandle(name));
+    }
+    catch (const hardware_interface::HardwareInterfaceException& e)
+    {
+      ROS_ERROR_STREAM("Exception: " << e.what());
+    }
+
+    urdf::JointConstSharedPtr joint_urdf = urdf.getJoint(name);
+    if (!joint_urdf)
+    {
+      ROS_ERROR("Could not find joint '%s' in urdf", name.c_str());
+      return false;
+    }
+    joint_urdfs_.push_back(joint_urdf);
+  }
+
+  controller_.resetInternalState();
 }
 
 template <class JointInterface>
 void RosControlInterface<JointInterface>::starting(const ros::Time& time)
 {
+  controller_.resetInternalState();
 }
 
 template <class JointInterface>
